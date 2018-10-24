ab82e5369b0f367f557f08d77acfc04a18bd667b
Ben Don <benbdon@gmail.com>
2018-10-24 08:53:49 -0500

    Added comment

--- algorithm practice/insertion_sort.cpp
+++ algorithm practice/insertion_sort.cpp
@@ -1,32 +1,34 @@
-/* Function to sort an array using insertion sort*/
-#include <cstdio>
-
+// Program that sorts an array
+#include <iostream>
 using namespace std;
 
-void insertion_sort(int arr[], int elementsOfArray) {
-	int i, j; // counters
-	for (i = 1; i < elementsOfArray; i++) {
+void insertion_sort(int arr[], int arraySize) {
+	int i; // progress through the full array
+	int j; // goes back through the previously reviewed elements
+	for (i = 1; i < arraySize; i++) {
 		j = i;
-		while ( (j > 0) && (arr[j] < arr[j-1])) { // j > 0 handles the 1 element array case
+		while ( (j > 0) && (arr[j] < arr[j-1])) {
 			arr[j + 1] = arr[j];
-			j = j - 1;
+			j -= 1;
 		}
 	}
 }
 
+// Utility to print out array
 void printArray(int arr[], int n) {
 	int i;
 	for (i = 0; i < n; i++)
-		printf("%d ", arr[i]);
-	printf("\n");
+		cout << arr[i] << " ";
+	cout << endl;
 }
 
+//Main program
 int main () {
 	int arr[] = { 12, 11, 13, 5, 6 };
-	int n = sizeof(arr) / sizeof(arr[0]);
+	int arraySize = sizeof(arr) / sizeof(arr[0]);
 
-	insertion_sort(arr, n);
-	printArray(arr, n);
+	insertion_sort(arr, arraySize);
+	printArray(arr, arraySize);
 
 	return 0;
 }
